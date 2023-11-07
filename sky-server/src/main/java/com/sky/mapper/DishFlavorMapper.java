319442e883af21d3e14db69127685ba7c214ef01
Tempted <2374704101@qq.com>
2023-11-07 20:20:37 +0800

    新增菜品代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.sky.annotation.AutoFill;
+import com.sky.entity.DishFlavor;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 菜品口味管理
+ * @Param
+ * @Return
+ **/
+@Mapper
+public interface DishFlavorMapper {
+
+    /**
+     * 批量插入口味数据
+     * @param flavors
+     */
+    void insertBatch(List<DishFlavor> flavors);
+}
