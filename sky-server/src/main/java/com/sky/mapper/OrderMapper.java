fa3c9f3640071e567b7e2acca0e19520b1f8fffb
Tempted <2374704101@qq.com>
2023-11-18 01:43:37 +0800

    订单状态定时处理代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     /**
@@ -45,4 +48,10 @@ public interface OrderMapper {
      */
     @Select("select count(*) from orders where status = #{status}")
     Integer countStatus(Integer status);
+
+    /**
+     * 查询超时未支付的订单
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{time}")
+    List<Orders> getByStatusAndOrderTime(Integer status, LocalDateTime time);
 }
---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,62 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * Author:tan hao
+ * Date: 2023-11-17 23:30
+ * Description:
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 处理超时订单的方法
+     */
+    @Scheduled(cron = "0 0/2 * * * ?")
+    public void processTimeoutOrder() {
+        log.info("定时处理超时订单：{}", LocalTime.now());
+//        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        LocalDateTime time = LocalDateTime.now().minusMinutes(15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTime(Orders.PENDING_PAYMENT, time);
+        if (ordersList != null && ordersList.size() > 0) {
+            ordersList.forEach(order -> {
+                order.setStatus(Orders.CANCELLED);
+                order.setCancelReason("支付超时，自动取消");
+                order.setCancelTime(LocalDateTime.now());
+                orderMapper.update(order);
+            });
+        }
+    }
+
+    /**
+     * 处理正在派送的订单
+     */
+    @Scheduled(cron = "0 0 * * * ? *")
+    public void processDeliveryOrder() {
+        log.info("定时处理派送中的订单，改成已完成状态");
+        // select * from orders where status = 4 and order_time < 当前时间-1小时
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTime(Orders.DELIVERY_IN_PROGRESS, time);
+        if (ordersList != null && ordersList.size() > 0) {
+            ordersList.forEach(order -> {
+                order.setStatus(Orders.COMPLETED);
+                orderMapper.update(order);
+            });
+        }
+    }
+}
